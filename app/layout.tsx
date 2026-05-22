171ae547d84bfcfb1fe2ed7b260d9d3ff94a7803
mistrihub <mistrihub75@gmail.com>
2026-05-22 08:00:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import Script from "next/script";
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://mistrihub.in";
-const gtmId = "GTM-NM3ZC5D2";
+const googleAnalyticsId = "G-8TR12XWYDJ";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
@@ -33,22 +33,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <noscript>
-  <iframe
-    src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-    height="0"
-    width="0"
-    style={{ display: "none", visibility: "hidden" }}
-  />
-</noscript>
+        <Script
+  src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+  strategy="afterInteractive"
+/>
 
-<Script id="google-tag-manager" strategy="afterInteractive">
+<Script id="google-analytics" strategy="afterInteractive">
   {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${gtmId}');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${googleAnalyticsId}');
   `}
 </Script>
         <SiteHeader />
